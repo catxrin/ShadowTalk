c0d8afba2dedebc6f6df75def9376ae07f698577
catxrin <catxrin@gmail.com>
2025-03-06 11:19:50 +0200

    improvements on the useFetch hook

--- src/helpers/useFetch.js
+++ src/helpers/useFetch.js
@@ -6,21 +6,17 @@ export default function useFetch({ url, body, method = 'GET' }) {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body),
   })
-    .then(async r => {
-      const text = await r.text();
-      const data = text ? JSON.parse(text) : {};
+    .then(async res => {
+      const text = await res.text();
+      const response = text ? JSON.parse(text) : {};
 
-      return { response: data, status: r.status };
-    })
-    .then(({ response, status }) => {
-      if (status < 200 || status > 299) {
-        enqueueSnackbar(response?.message, { variant: 'error', autoHideDuration: 2000 });
-        throw new Error(response?.message);
+      if (!response.ok) {
+        throw new Error(response?.message || `Error ${response?.status}`);
       }
       return response;
     })
     .catch(err => {
       enqueueSnackbar(err?.message, { variant: 'error', autoHideDuration: 2000 });
-      throw err;
+      throw new Error(err);
     });
 }
