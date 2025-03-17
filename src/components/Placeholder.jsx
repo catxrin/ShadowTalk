80db315eaf6aabfde109da88969b793e186b3ea2
catxrin <catxrin@gmail.com>
2025-03-17 14:18:59 +0200

    created the user search

--- src/components/Placeholder.jsx
+++ src/components/Placeholder.jsx
@@ -1,6 +1,6 @@
 export default function Placeholder() {
   return (
-    <div className='flex items-center justify-center w-full'>
+    <div className='flex items-center justify-center w-full h-full'>
       <p className='text-gray-300 xl:text-lg text-sm text-center'>No chat selected. Start a conversation!</p>
     </div>
   );
---
+++ src/components/ProfileHeader.jsx
@@ -0,0 +1,42 @@
+import Icon from './Icon';
+import { useContext } from 'react';
+import { UserContext } from '../UserProvider';
+import { useState } from 'react';
+import Settings from '../views/private/Settings';
+import { useNavigate } from 'react-router-dom';
+
+import { logout } from '../helpers/auth';
+export default function ProfileHeader() {
+  const { user, setUserAuth } = useContext(UserContext);
+  const [show, setShow] = useState(false);
+
+  const navigate = useNavigate();
+
+  const username = user?.username.length > 10 ? `${user?.username.substring(0, 10)}...` : user?.username;
+
+  const logoutUser = () => {
+    logout().then(() => {
+      setUserAuth(null);
+      navigate('/');
+    });
+  };
+  return (
+    <>
+      {show && <Settings setShow={setShow} />}
+      <div className='bg-[#1C1D22] text-white p-4 flex flex-row justify-between'>
+        <div className='flex flex-row items-center gap-2 font-semibold'>
+          <img
+            className='rounded-full border border-gray-500 w-10 object-cover'
+            src={`/server/${user?.image}`}
+            alt='pfp'
+          />
+          <p className='text-sm'>{username}</p>
+        </div>
+        <div className='flex flex-row items-center gap-4 text-gray-300'>
+          <Icon onClick={() => setShow(true)} styles='!text-2xl' icon='settings' />
+          <Icon onClick={logoutUser} styles='!text-2xl' icon='logout' />
+        </div>
+      </div>
+    </>
+  );
+}
